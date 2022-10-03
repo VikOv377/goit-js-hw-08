7ed7c5f0eff95d31673609b13950b620e9d3acab
Viktoriya Ovcharenko <vickymmm2012@gmail.com>
2022-10-03 15:45:35 +0200

    Video trial #1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+/*Ініціалізація існуючого плеєра*/
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+/* Використання методу on, відслідження часу оновлення програвання timeupdate,
+зберігання часу програвання в localStorage. */
+
+player.on('timeupdate', throttle(onUpdatedTime, 1000));
+
+function onUpdatedTime ({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds)
+  };
+
+/*Використання методу setCurrentTime() при перезгрузці сторінки, для того щоб відео розпочиналось зі 
+збереженого моменту */
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .catch(function (error) {
+    console.error(error)
+});
+ 
