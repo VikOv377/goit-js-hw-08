8648ae787c447865ff66302d8f1d29de063a0fa1
Viktoriya Ovcharenko <vickymmm2012@gmail.com>
2022-10-03 17:42:54 +0200

    Form trial #1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener
+(
+    'input', 
+    throttle(event => {
+        const dataToSave = { email: email.value, message: message.value }
+        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataToSave));
+    }, 500)
+
+);
+
+form.addEventListener
+(
+    'submit',
+     event => {
+        event.preventDefault();
+
+        console.log({ email: email.value, message: message.value });
+        
+        const {
+            elements: { email, message },
+          } = event.currentTarget;
+          
+        event.currentTarget.reset();
+        
+        localStorage.remove(LOCALSTORAGE_KEY);
+     }
+);
+
+const storageData = localStorage.getItem(LOCALSTORAGE_KEY);
+const parsedStorage = JSON.parse(storageData);
+
+const fillUpForm = () => {
+    if (parsedStorage !== null) {
+      input.value = parseStorage.email;
+      textarea.value = parseStorage.message;
+    }
+  };
+
+fillUpForm();
